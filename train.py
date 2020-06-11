74ff5ec692d2cdff2d01db3157adedbe1d207f57
oToToT <ty1208chiang@gmail.com>
2020-06-11 13:36:49 +0800

    wtf

--- train.py
+++ train.py
@@ -57,6 +57,29 @@ def path_join(arr):
 from torch import nn
 from torch.autograd import Variable
 
+class fcn_autoencoder(nn.Module):
+    def __init__(self):
+        super(fcn_autoencoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(32 * 32 * 3, 128),
+            nn.ReLU(True),
+            nn.Linear(128, 64),
+            nn.ReLU(True), nn.Linear(64, 12), nn.ReLU(True), nn.Linear(12, 3))
+        self.decoder = nn.Sequential(
+            nn.Linear(3, 12),
+            nn.ReLU(True),
+            nn.Linear(12, 64),
+            nn.ReLU(True),
+            nn.Linear(64, 128),
+            nn.ReLU(True), nn.Linear(128, 32 * 32 * 3
+            ), nn.Tanh())
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+fcn_autoencoder()
+
 class conv_autoencoder(nn.Module):
     def __init__(self):
         super(conv_autoencoder, self).__init__()
